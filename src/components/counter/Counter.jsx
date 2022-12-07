350013b9ee86ccf5ad2f8b5b2eefd5cb5b05115f
urtesala <urtesaladziute@gmail.com>
2022-12-07 10:44:19 +0300

    counter with useReducer

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,33 +1,56 @@
-import { useState } from 'react';
+import { useReducer, useState } from 'react';
 
 const initCounter = { value: 0 };
 
+function counterReducer(state, action) {
+  switch (action.type) {
+    case 'upBy':
+      return { value: state.value + action.payload };
+    case 'up':
+      return { value: state.value + 1 };
+    case 'down':
+      return { value: state.value - 1 };
+    case 'reset':
+      return { value: 0 };
+    default:
+      // throw new Error('tokio action tipo nera');
+      console.warn('tokio action tipo nera');
+      return initCounter;
+  }
+}
+
 function Counter(props) {
   // sukurti state counterValue
-  const [counterState, setCounterState] = useState(initCounter);
+  //   const [counterState, setCounterState] = useState(initCounter);
+  const [state, dispatch] = useReducer(counterReducer, initCounter);
 
   // sukurti fn handleUp, handleDown, handleUpBy, handleReset
   function handleUp() {
-    setCounterState((prevCountState) => ({ value: prevCountState.value + 1 }));
+    // setCounterState((prevCountState) => ({ value: prevCountState.value + 1 }));
     // setCounterValue(counterValue + 1); // not good
+
+    dispatch({ type: 'up' });
   }
   function handleDown() {
-    setCounterState((prevCountState) => ({ value: prevCountState.value - 1 }));
+    // setCounterState((prevCountState) => ({ value: prevCountState.value - 1 }));
+
+    dispatch({ type: 'down' });
   }
   function handleUpBy(howMuch) {
-    setCounterState((prevCountState) => ({
-      value: prevCountState.value + howMuch,
-    }));
+    // setCounterState((prevCountState) => ({
+    //   value: prevCountState.value + howMuch,
+    // }));
+    dispatch({ type: 'upBy', payload: howMuch });
   }
   function handleReset() {
-    setCounterState(initCounter);
+    // setCounterState(initCounter);
+    dispatch({ type: 'reset' });
   }
-  // sudeti fn ant mygtuku kad veiktu aplikaicija
 
   return (
     <div className='card counter'>
       <p>Counter</p>
-      <h2>{counterState.value}</h2>
+      <h2>{state.value}</h2>
       <div className='ctrl'>
         <button onClick={handleUp}>up</button>
         <button onClick={handleDown}>down</button>
