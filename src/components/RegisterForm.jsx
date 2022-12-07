0d3e422c32e5587129ef50cebbd0e1a6b19062ae
urtesala <urtesaladziute@gmail.com>
2022-12-07 15:43:38 +0300

    fetching form

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -41,54 +41,75 @@ function RegisterForm(props) {
 
     if (doPasswordsAreEqual) {
       dispatch({ type: 'feedback', payload: 'Passwords match OK' });
+      // 1P. siusti su fetch i https://reqres.in/api/register
+      // objekta kuris turi email ir password
+      const newUserObj = {
+        email: state.email,
+        password: state.password,
+      };
+      registerUserFetch(newUserObj);
+      // 2P. jei gaunam sekminga atsakyma, paslepti forma ir parodyti sekmes kortele kurioje atspausdinta tokenas ir userio id.
+      // 3P. jei atsakymas nesekmmingas, tai virs formos pranesame kokia klaida is atsakymo.
     } else {
       dispatch({
         type: 'feedback',
         payload: 'please check passwords do not match!!!',
       });
     }
-  };
 
-  return (
-    <div>
-      <h2>Register here</h2>
-      <form onSubmit={submitHandler} className='card'>
-        <input
-          onChange={(e) => dispatch({ type: 'email', payload: e.target.value })}
-          value={state.email}
-          type='text'
-          placeholder='email'
-        />
-        <h3>{state.feedback}</h3>
-        <input
-          onChange={(e) =>
-            dispatch({ type: 'password', payload: e.target.value })
-          }
-          value={state.password}
-          type='password'
-          placeholder='password'
-        />
-        <input
-          onChange={(e) =>
-            dispatch({ type: 'repeatPassword', payload: e.target.value })
-          }
-          value={state.repeatPassword}
-          type='password'
-          placeholder='repeat password'
-        />
-        <button type='submit'>Login</button>
-      </form>
-      {!props.hideDebug && (
-        <>
-          <hr />
-          <h3>Debug values</h3>
-          <p>Email: {state.email}</p>
-          <p>Password: {state.password}</p>
-          <p>Repeat Password: {state.repeatPassword}</p>
-          <p>Do they match: {doPasswordsAreEqual.toString()}</p>
-        </>
-      )}
-    </div>
-  );
+    return (
+      <div>
+        <h2>Register here</h2>
+        <form onSubmit={submitHandler} className='card'>
+          <input
+            onChange={(e) =>
+              dispatch({ type: 'email', payload: e.target.value })
+            }
+            value={state.email}
+            type='text'
+            placeholder='email'
+          />
+          <h3>{state.feedback}</h3>
+          <input
+            onChange={(e) =>
+              dispatch({ type: 'password', payload: e.target.value })
+            }
+            value={state.password}
+            type='password'
+            placeholder='password'
+          />
+          <input
+            onChange={(e) =>
+              dispatch({ type: 'repeatPassword', payload: e.target.value })
+            }
+            value={state.repeatPassword}
+            type='password'
+            placeholder='repeat password'
+          />
+          <button type='submit'>Login</button>
+        </form>
+        {!props.hideDebug && (
+          <>
+            <hr />
+            <h3>Debug values</h3>
+            <p>Email: {state.email}</p>
+            <p>Password: {state.password}</p>
+            <p>Repeat Password: {state.repeatPassword}</p>
+            <p>Do they match: {doPasswordsAreEqual.toString()}</p>
+          </>
+        )}
+      </div>
+    );
+  };
 }
 export default RegisterForm;
+
+async function registerUserFetch(userObj) {
+  const resp = await fetch('https://reqres.in/api/register', {
+    method: 'POST',
+    headers: { 'Content-type': 'application/json' },
+    body: JSON.stringify(userObj),
+  });
+  const responseInJs = await resp.json();
+  console.log('responseInJs ===', responseInJs);
+}
